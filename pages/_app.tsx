d6ef288e0456f1da5322faa41358ae4f3a7372e4
Jorge Jimenez <jjimenezdiaz@quental.com>
2022-12-22 22:31:41 +0100

    add dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
+import { darkTheme } from '../themes'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   )
