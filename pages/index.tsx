763830fe7b19596ab82b32074c1e222fe0ab879e
Jorge Jimenez <jjimenezdiaz@quental.com>
2022-12-22 22:17:53 +0100

    install nextui library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react'
 import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Home Page...</h1>
+      <Button color={'gradient'}>Button Text</Button>
     </>
   )
 }
